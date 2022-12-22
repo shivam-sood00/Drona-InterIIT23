0974dd959963ee256103f9cb0503cd3246bcd187
aditya-jha13 <jha.aditya2121@gmail.com>
2022-12-22 22:51:42 +0530

    Testing drone with throttle

--- test/test.py
+++ test/test.py
@@ -1,14 +1,21 @@
 # Code for testing the communication with drone
 
 import socket
+import comms
+import time
 #if camera connected:
 # TCP_IP = '192.168.0.1'
 # TCP_PORT = 9060
 
 #if not
-TCP_IP = '192.168.4.1'
-TCP_PORT = 23
-mysocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-print("Trying to connect")
-mysocket.connect((TCP_IP,TCP_PORT)) # A successful connection has been setup
-print ("Connected to drone")
+# TCP_IP = '192.168.4.1'
+# TCP_PORT = 23
+
+myDrone = comms.comms('192.168.4.1',23,True)
+myDrone.arm()
+myDrone.setThrottle(1000)
+myDrone.setThrottle(1050)
+time.sleep(3)
+myDrone.disarm()
+myDrone.disconnect()
+
