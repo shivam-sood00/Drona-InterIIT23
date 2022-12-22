d638b71c376be923874b2893d8a0dd51d0d29c3e
to-the-abyss <shivamsood2000@gmail.com>
2022-12-22 20:04:47 +0530

    socket value changed to integer, cammera connection ip added

--- test/test.py
+++ test/test.py
@@ -1,8 +1,14 @@
 # Code for testing the communication with drone
 
 import socket
+#if camera connected:
+# TCP_IP = '192.168.0.1'
+# TCP_PORT = 9060
+
+#if not
 TCP_IP = '192.168.4.1'
-TCP_PORT = '23'
+TCP_PORT = 23
 mysocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print("Trying to connect")
 mysocket.connect((TCP_IP,TCP_PORT)) # A successful connection has been setup
 print ("Connected to drone")
