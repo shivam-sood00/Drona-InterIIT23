5bcde611e246a5fc9de4cabb570f1fa8e9624805
Aman Kumar Singh <amanmain3@gmail.com>
2023-01-02 17:51:54 +0530

    Update ekf_xyz.py

--- POSEtransform/python_env/ekf_xyz.py
+++ POSEtransform/python_env/ekf_xyz.py
@@ -1,32 +1,71 @@
 import numpy as np
  
 
-
+# Description: Extended Kalman Filter example (two-wheeled mobile robot)
 
 np.set_printoptions(precision=3,suppress=True)
- 
+g = 9.81 # accleration due to gravity
+m = 1.0
+ix = 1.0
+iy = 1.0
+iz = 1.0
 # A matrix
 # A is sometimes F in the literature.
-A_k_minus_1 = np.array([[1.0,  0,   0],
-                        [  0,1.0,   0],
-                        [  0,  0, 1.0]])
- 
+
+def A_k_minus_1(dt):
+    return np.array([[1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -g, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, g, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 1.0, 0, 0, 0, 0, 0, 0, 0, 0,],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.0, 0, 0, 0, 0, 0, 0],
+                            [1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.0, 0, 0, 0, 0, 0, 0],
+                            [1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.0, 0, 0, 0, 0, 0],
+                            [1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.0, 0, 0, 0, 0],
+                            [1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])*dt + np.eye(18)
+
+def B_k_minus_1(dt):
+    B = np.array([[0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [1/m, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 1/ix, 0, 0],
+                [0, 0, 1/iy, 0],
+                [0, 0, 0, 1/iz]])*dt
+
 # Noise applied to the forward kinematics (calculation
 # with the number of elements equal to the number of states
 
-process_noise_v_k_minus_1 = np.array([0.0001,0.0001,0.0003])
+process_noise_v_k_minus_1 = np.array([0.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
      
 # State model noise covariance matrix Q_k
 # Q is a square matrix that has the same number of rows as states.
-Q_k = np.array([[1.0,   0,   0],
-                [  0, 1.0,   0],
-                [  0,   0, 1.0]])
+Q_k = np.eye(18, dtype=np.float32)
                  
 # Measurement matrix H_k
 # H has the same number of rows as sensor measurements and same number of columns as states.
-H_k = np.array([[1.0,  0,   0],
-                [  0,1.0,   0],
-                [  0,  0, 1.0]])
+H_k = np.array([[1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
                          
 # Sensor measurement noise covariance matrix R_k
 # If we are sure about the measurements, R will be near zero.
@@ -36,37 +75,33 @@ R_k = np.array([[1.0,   0,    0],
                  
 # Sensor noise. This is a vector with the
 # number of elements equal to the number of sensor measurements.
-sensor_noise_w_k = np.array([0.001,0.001,0.001])
+sensor_noise_w_k = np.array([0.07,0.07,0.04])
+sensor_noise_w_k_2 = np.array([0.07,0.07,0.04])
  
-def getB():
-   
-    B = np.array([  [0, 0, 0, 0],
-                    [0, 0, 0, 0],
-                    [0, 0, 0, 0]])
-    return B
  
-def ekf(z_k_observation_vector, state_estimate_k_minus_1, 
+def ekf(z_k_observation_vector, z_k_observation_vector_2, state_estimate_k_minus_1, 
         control_vector_k_minus_1, P_k_minus_1, dk):
     
     ######################### Predict #############################
     # Predict the state estimate at time k based on the state 
     # estimate at time k-1 and the control input applied at time k-1.
-    # Ax+Bu+noise
-    state_estimate_k = A_k_minus_1 @ (
-            state_estimate_k_minus_1) + (
-            getB()) @ (control_vector_k_minus_1) + (process_noise_v_k_minus_1)
+    state_estimate_k = A_k_minus_1(dk) @ (
+            state_estimate_k_minus_1) + (B_k_minus_1(dk)) @ (control_vector_k_minus_1) + (
+            process_noise_v_k_minus_1)
              
     print(f'State Estimate Before EKF={state_estimate_k}')
              
     # Predict the state covariance estimate based on the previous
     # covariance and some noise
-    P_k = A_k_minus_1 @ P_k_minus_1 @ A_k_minus_1.T + (
-            Q_k)
+    P_k = A_k_minus_1(dk) @ P_k_minus_1 @ A_k_minus_1(dk).T + (Q_k)
          
     ################### Update (Correct) ##########################
     # Calculate the difference between the actual sensor measurements
     # at time k minus what the measurement model predicted 
     # the sensor measurements would be for the current timestep k.
+
+
+    
     measurement_residual_y_k = z_k_observation_vector - (
             (H_k @ state_estimate_k) + (
             sensor_noise_w_k))
@@ -85,9 +120,17 @@ def ekf(z_k_observation_vector, state_estimate_k_minus_1,
      
     # Update the state covariance estimate for time k
     P_k = P_k - (K_k @ H_k @ P_k)
-     
+    print(f'State Estimate After first update={state_estimate_k}')
+     ################### Update Again for another sensor##########################
+    measurement_residual_y_k_2 = z_k_observation_vector_2 - (
+            (H_k @ state_estimate_k) + (
+            sensor_noise_w_k_2))
+    S_k = H_k @ P_k @ H_k.T + R_k
+    K_k = P_k @ H_k.T @ np.linalg.pinv(S_k)
+    state_estimate_k = state_estimate_k + (K_k @ measurement_residual_y_k_2)
+    P_k = P_k - (K_k @ H_k @ P_k)
     # Print the best (near-optimal) estimate of the current state of the robot
-    print(f'State Estimate After EKF={state_estimate_k}')
+    print(f'State Estimate After second update={state_estimate_k}')
  
     # Return the updated state and covariance estimates
     return state_estimate_k, P_k
@@ -99,20 +142,27 @@ def main():
      
     # Time interval in seconds
     dk = 1
- 
+    
+    # fOR ARYCO sensor say
     # Create a list of sensor observations at successive timesteps
     # Each list within z_k is an observation vector.
-    # For testing purpose
     z_k = np.array([[4.721,0.143,0.006], # k=1
-                    [9.353,0.284,0.007], # k=2
-                    [14.773,0.422,0.009],# k=3
-                    [18.246,0.555,0.011], # k=4
-                    [22.609,0.715,0.012]])# k=5
+                    [9.353,0.284,0.007,], # k=2
+                    [14.773,0.422,0.009,],# k=3
+                    [18.246,0.555,0.011,], # k=4
+                    [22.609,0.715,0.012,]])# k=5
+
+    #FoR yelo+depth sensor    
+    z_k_2 = np.array([[4.734,0.155,0.007], # k=1
+                    [9.388,0.265,0.006,], # k=2
+                    [14.767,0.454,0.009,],# k=3
+                    [18.234,0.598,0.012,], # k=4
+                    [22.698,0.722,0.013,]])# k=5
                      
     # The estimated state vector at time k-1 in the global reference frame.
     # [x_k_minus_1, y_k_minus_1, yaw_k_minus_1]
     # [meters, meters, radians]
-    state_estimate_k_minus_1 = np.array([0.0,0.0,0.0])
+    state_estimate_k_minus_1 = np.array([4.721,0.143,0.006, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
      
     # The control input vector at time k-1 in the global reference frame.
     # [v, yaw_rate]
@@ -121,27 +171,28 @@ def main():
     # Because there is no angular velocity and the robot begins at the 
     # origin with a 0 radians yaw angle, this robot is traveling along 
     # the positive x-axis in the global reference frame.
-    control_vector_k_minus_1 = np.array([4.5,0.0,0.6, 0.6])
+    control_vector_k_minus_1 = np.array([1.0,1.0, 1.0, 1.0])
      
     # State covariance matrix P_k_minus_1
     # This matrix has the same number of rows (and columns) as the 
-    # number of states (i.e. 3x3 matrix). It represents an estimate of 
+    # number of states (i.e. 3x3 matrix). P is sometimes referred
+    # to as Sigma in the literature. It represents an estimate of 
     # the accuracy of the state estimate at time k made using the
-    # state transition matrix. [We start off with guessed values.]
-    P_k_minus_1 = np.array([[0.1,  0,   0],
-                            [  0,0.1,   0],
-                            [  0,  0, 0.1]])
+    # state transition matrix. We start off with guessed values.
+    P_k_minus_1 = 0.01*np.eye(18, dtype=np.float32)
                              
-    # step for vision observation input and contorl input goes here
-    for k, obs_vector_z_k in enumerate(z_k,start=1):
-     
+    # Start at k=1 and go through each of the 5 sensor observations, 
+    # one at a time. 
+    # We stop right after timestep k=5 (i.e. the last sensor observation)
+    for k, obs_vector_z_k in enumerate(z_k, start=1):
+        # print(z_k_2[k-1].shape)
         # Print the current timestep
         print(f'Timestep k={k}')  
          
         # Run the Extended Kalman Filter and store the 
         # near-optimal state and covariance estimates
         optimal_state_estimate_k, covariance_estimate_k = ekf(
-            obs_vector_z_k, # Most recent sensor measurement
+            obs_vector_z_k, z_k_2[k-1],# Most recent sensor measurement both sensor 1 and sensor 2
             state_estimate_k_minus_1, # Our most recent estimate of the state
             control_vector_k_minus_1, # Our most recent control input
             P_k_minus_1, # Our most recent state covariance matrix
@@ -153,6 +204,5 @@ def main():
          
         # Print a blank line
         print()
- 
-# Program starts running here with the main method  
+  
 main()
