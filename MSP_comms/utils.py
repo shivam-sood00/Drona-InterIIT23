3f573ea1541516c1e5702ce5928d58992397fc90
12deeps <104849485+12deeps@users.noreply.github.com>
2023-01-25 23:59:09 +0530

    Add files via upload

---
+++ MSP_comms/utils.py
@@ -0,0 +1,29 @@
+import math
+
+def getBytes(value): 
+    LSB=value % 256
+    MSB=math.floor(value/256)
+    return [LSB,MSB]   
+
+def getDec(lsb,msb,base=256):
+    return lsb+base*msb
+
+def getSignedDec(lsb,msb):
+    a = getDec(lsb,msb)
+    if msb>127:
+        a = -2**16+a
+    return a
+
+def getCRC(arr,debug=False):
+    CRCArray=arr[3:]
+    if debug:
+        print("CRC arrary = ",list(CRCArray))
+    CRCValue=0
+    for d in CRCArray:
+        CRCValue= CRCValue^d
+    return CRCValue
+
+def toUnsigned(val):
+    if val<0:
+        return 2**16 + val
+    return val
